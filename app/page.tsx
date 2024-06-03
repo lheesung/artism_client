761c62627e0e92f58f9496bce598cb0f20c2f505
lheesung <lheesung.dev@gmail.com>
2024-06-03 15:05:59 +0900

    refactor: styled-component로 바꿈

--- app/page.tsx
+++ app/page.tsx
@@ -8,32 +8,54 @@ import Button from '@/styles/ui/button';
 import { useRouter } from 'next/navigation';
 import { getRandomInt } from '@/utils/number';
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+
+interface ContainerProps {
+  background: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  background: ${({ background }) =>
+    `linear-gradient(to bottom, ${background}, #ffffff)`};
+  align-items: center;
+  justify-content: center;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+  cursor: grab;
+`;
 
 export default function Home() {
   const { push } = useRouter();
-  const [logoNumber, setLogoNumber] = useState(0);
+  const [randNumber, setRandNumber] = useState(0);
   const Logos = [Logo01, Logo02, Logo03];
+  const colors = ['#FFCD00', '#E45556', '#5C9582'];
 
   useEffect(() => {
-    setLogoNumber(getRandomInt(0, 2));
+    setRandNumber(getRandomInt(0, 2));
   }, []);
 
   return (
-    <div
-      className='
-        flex w-[100vw] h-[100vh] bg-gradient-to-b from-[#FFCD00] to-[#FFFFFF]
-        items-center justify-center
-      '
-    >
-      <div className='flex flex-col items-center gap-8'>
-        <Image src={Logos[logoNumber]} alt={'artism'} width={400} />
-        <h1 className='text-2xl font-bold cursor-grab'>
-          아티즘에서 영감을 나누세요.
-        </h1>
+    <Container background={colors[randNumber]}>
+      <Content>
+        <Image src={Logos[randNumber]} alt={'artism'} width={400} />
+        <Title>아티즘에서 영감을 나누세요.</Title>
         <Button onClickButton={() => push('/gallery')} isColored>
           아티즘 입장하기
         </Button>
-      </div>
-    </div>
+      </Content>
+    </Container>
   );
 }
---
+++ constants/galleryContents.ts
@@ -0,0 +1,1 @@
+export const contentsType = ['MONTH', 'PHOTO', 'POEM'];
