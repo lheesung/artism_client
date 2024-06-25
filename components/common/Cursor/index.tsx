c736701cd6c20e40e259ed2a0c2ebfdfa9a44542
lheesung <lheesung.dev@gmail.com>
2024-06-25 09:41:45 +0900

    add: cursor change on button hover

--- components/common/Cursor/index.tsx
+++ components/common/Cursor/index.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { font } from '@/styles/base/font';
+import { Row } from '@/styles/ui/flex';
 import { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Cursor = styled.div`
+const Cursor = styled.div<{ isOverButton: boolean }>`
   width: 30px;
   height: 30px;
   background-color: #fff;
@@ -14,14 +16,34 @@ const Cursor = styled.div`
   border-radius: 50%;
   will-change: transform;
   z-index: 9999;
+  transition: width 0.3s ease, height 0.3s ease, border-radius 0.3s ease;
+  ${({ isOverButton }) =>
+    isOverButton &&
+    css`
+      width: 80px;
+      height: 40px;
+      border-radius: 50px;
+      ${font.xl}
+    `}
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const CustomCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isOverButton, setIsOverButton] = useState(false);
 
   useEffect(() => {
     const handleMouseMove = (event: MouseEvent) => {
       setPosition({ x: event.clientX, y: event.clientY });
+
+      const element = document.elementFromPoint(event.clientX, event.clientY);
+      if (element && element.tagName === 'BUTTON') {
+        setIsOverButton(true);
+      } else {
+        setIsOverButton(false);
+      }
     };
 
     window.addEventListener('mousemove', handleMouseMove);
@@ -31,7 +53,14 @@ const CustomCursor = () => {
     };
   }, []);
 
-  return <Cursor style={{ top: `${position.y}px`, left: `${position.x}px` }} />;
+  return (
+    <Cursor
+      isOverButton={isOverButton}
+      style={{ top: `${position.y}px`, left: `${position.x}px` }}
+    >
+      {isOverButton && 'view'}
+    </Cursor>
+  );
 };
 
 export default CustomCursor;
